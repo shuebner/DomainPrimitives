c5a69e641b505af52fa05c2037f7009a077edec5
Sven Hübner <sven.huebner.it@gmx.de>
2020-06-01 20:31:56 +0200

    add KindOf as base class for reference types

---
+++ DomainPrimitives.UnitTests/KindOfTTests.cs
@@ -0,0 +1,120 @@
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace SvSoft.DomainPrimitives
+{
+    public class OfTTests
+    {
+        [Fact]
+        public void Can_be_used_as_underlying_type()
+        {
+            FirstName firstName = new FirstName("John");
+
+            string firstNameStr = firstName;
+
+            firstNameStr.Should().Be("John");
+        }
+
+        [Fact]
+        public void Can_not_be_used_as_different_type_with_same_underlying_type()
+        {
+            FirstName firstName = new FirstName("John");
+
+            firstName.Should().NotBeAssignableTo<LastName>();
+        }
+
+        [Fact]
+        public void Ctor_When_underlying_value_is_null_Then_throws_ArgumentNullException()
+        {
+            Action act = () => new FirstName(null);
+
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Equals_When_types_are_same_and_underlying_value_is_same_Then_returns_true()
+        {
+            FirstName john1 = new FirstName("John");
+            FirstName john2 = new FirstName("John");
+
+            var isEqual = john1.Equals(john2);
+
+            isEqual.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Equals_When_types_are_same_and_underlying_values_are_different_Then_returns_false()
+        {
+            FirstName john = new FirstName("John");
+            FirstName jane = new FirstName("Jane");
+
+            var isEqual = john.Equals(jane);
+
+            isEqual.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Equals_When_types_are_different_and_underlying_value_is_same_Then_returns_false()
+        {
+            FirstName john1 = new FirstName("John");
+            LastName john2 = new LastName("John");
+
+            var isEqual = john1.Equals(john2);
+
+            isEqual.Should().BeFalse();
+        }
+
+        [Fact]
+        public void GetHashCode_When_underlying_value_is_different_Then_is_likely_different()
+        {
+            var john = new FirstName("John");
+            var jane = new FirstName("Jane");
+
+            var johnHash = john.GetHashCode();
+            var janeHash = jane.GetHashCode();
+
+            johnHash.Should().NotBe(janeHash);
+        }
+
+        [Fact]
+        public void GetHashCode_When_type_and_underlying_value_is_same_Then_is_same()
+        {
+            var john1 = new FirstName("John");
+            var john2 = new FirstName("John");
+
+            var john1Hash = john1.GetHashCode();
+            var john2Hash = john2.GetHashCode();
+
+            john1Hash.Should().Be(john2Hash);
+        }
+
+        [Fact]
+        public void GetHashCode_When_types_are_different_And_underlying_value_is_same_Then_is_likely_different()
+        {
+            FirstName john1 = new FirstName("John");
+            LastName john2 = new LastName("John");
+
+            var john1Hash = john1.GetHashCode();
+            var john2Hash = john2.GetHashCode();
+
+            john1Hash.Should().NotBe(john2Hash);
+        }
+
+        private class FirstName : KindOf<string>
+        {
+            public FirstName(string value)
+                : base(value)
+            {
+            }
+        }
+
+        private class LastName : KindOf<string>
+        {
+            public LastName(string value)
+                : base(value)
+            {
+            }
+        }
+    }
+}
---
+++ DomainPrimitives/KindOfT.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SvSoft.DomainPrimitives
+{
+    public abstract class KindOf<T>
+        where T : class
+    {
+        private readonly T Value;
+
+        public KindOf(T value)
+        {
+            Value = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public override bool Equals(object obj) =>
+            obj != null &&
+            obj.GetType() == GetType() &&
+            Equals(Value, ((KindOf<T>)obj).Value);
+
+        public override int GetHashCode() =>
+            GetType().GetHashCode() * 31 + Value.GetHashCode();
+
+        public static implicit operator T(KindOf<T> kindOfT) => kindOfT.Value;
+    }
+}
