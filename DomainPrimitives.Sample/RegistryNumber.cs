2c7d9554508cbaf7fa0614730f687a2c8aa910a5
Sven Hübner <sven.huebner.it@gmx.de>
2020-11-19 13:31:40 +0100

    add RegistryNumber as example

---
+++ DomainPrimitives.Sample/RegistryNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SvSoft.DomainPrimitives.Sample
+{
+    public sealed class RegistryNumber : KindOf<string>
+    {
+        public RegistryNumber(string value)
+            : base(value)
+        {
+            if (!Regex.IsMatch(value, "^[0-9|A-Z]{9}$"))
+            {
+                throw new ArgumentException("must be 9-digit alphanumeric string", nameof(value));
+            }
+        }
+    }
+}
---
+++ DomainPrimitives.UnitTests/RegistryNumberTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using SvSoft.DomainPrimitives.Sample;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SvSoft.DomainPrimitives
+{
+    public class RegistryNumberTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("abc")]
+        [InlineData("12345678")]
+        [InlineData("1234567890")]
+        [InlineData("1234567-8")]
+        public void Ctor_When_value_is_invalid_Then_throws_ArgumentException(string value)
+        {
+            Action act = () => new RegistryNumber(value);
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Theory]
+        [InlineData("123456789")]
+        [InlineData("ABCDEFGHI")]
+        [InlineData("1B3D5F7H9")]
+        public void Ctor_When_value_is_valid_Then_does_not_throw(string value)
+        {
+            Action act = () => new RegistryNumber(value);
+
+            act.Should().NotThrow();
+        }
+    }
+}
